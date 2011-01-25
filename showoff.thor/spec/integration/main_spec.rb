b5a0474be3d32b9f35f71a9d3d33cb4b6b4edbed
Anders Janmyr <anders@janmyr.com>
2011-01-25 17:51:51 +0100

    Added more specs

--- showoff.thor/spec/integration/main_spec.rb
+++ showoff.thor/spec/integration/main_spec.rb
@@ -21,7 +21,6 @@ describe 'thor showoff' do
       clean
       thor('showoff dummy')
     end
-
    
     it 'should create the expected files' do
       file('dummy/abstract.md').should exist
@@ -34,9 +33,29 @@ describe 'thor showoff' do
       file('dummy/slides/99_summary.md').should exist
     end
 
-    it 'should create "abstract.md" with a title' do
-      file('dummy/abstract.md').contents.should match(/## dummy/)
+    context 'abstract.md' do
+      it 'should contain title' do
+        file('dummy/abstract.md').contents.should match(/## dummy/)
+      end
     end
+
+    context 'showoff.json' do
+      it 'should contain the name' do
+        file('dummy/showoff.json').contents.should match(/"dummy"/)
+      end
+    end
+    
+    context '01_intro.md' do
+      it 'should contain the name' do
+        file('dummy/slides/01_intro.md').contents.should match(/# dummy #/)
+      end
+
+      it 'should not contain sub_title' do
+        file('dummy/slides/01_intro.md').contents.should_not match(/sub_title/)
+      end
+    end
+    
+    
     
     
   end
