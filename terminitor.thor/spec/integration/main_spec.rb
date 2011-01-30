335d9fa4c748f9aeee56082c2afade27d439157a
Anders Janmyr <anders@janmyr.com>
2011-01-30 17:38:44 +0100

    fixed the specs

--- terminitor.thor/spec/integration/main_spec.rb
+++ terminitor.thor/spec/integration/main_spec.rb
@@ -1,22 +1,27 @@
 require File.join(File.dirname(__FILE__), '../spec_helper')
 
 describe 'thor terminitor' do
-  
-  describe 'running with NAME argument' do
+ 
+  let(:dummy_file) {File.new("#{ENV['HOME']}/.terminitor/dummy.term")}
+
+  describe 'running rails with NAME argument' do
    
     it 'should create the expected files' do
       thor('terminitor rails dummy')
-      File.new('~/.terminitor/dummy.term').should exist
+      dummy_file.should exist
     end
-
+  end
+  
+  describe 'running jquery with NAME argument' do
     it 'should create the expected files' do
       thor('terminitor jquery dummy')
-      File.new('~/.terminitor/dummy.term').should exist
+      dummy_file.should exist
     end
 
-    after do
-      rm_rf '~/.terminitor/dummy.term'
-    end
-  
   end
+
+  after :each do
+    rm_rf dummy_file
+  end
+  
 end
